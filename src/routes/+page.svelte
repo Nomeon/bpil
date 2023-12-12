f7a669a2ef5b3b588204cb6f819d6e3716200be4
s2358034 <s.p.g.nijhuis@student.utwente.nl>
2023-12-12 17:09:03 +0100

    Create guide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,13 @@
 
 </script>
 <div>
-    <h2>Data</h2>
-    <p>{JSON.stringify(data)}</p>
+    <h1>Testable CT API Endpoints:</h1>
+    <ul>
+        <li>
+            <h2>Location Request</h2>
+            <p id='call'>https://bpil.vercel.app/api/locationRequest</p>
+            <p id='method'>Method: GET</p>
+            <p id='params'>Params: OrderID</p>
+            <p id='example'>Example: https://bpil.vercel.app/api/locationRequest?OrderID="6"</p>
+        </li>
 </div>
